fa6adcd28d2a54b486185da2da686fad07cb3946
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-03-10 22:41:30 +0300

    The frontend side project coded. Navbar and Student scripts created and listed on page

---
+++ studentfrontend/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+
+export default function () {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Spring Boot React Full Stack Application
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
